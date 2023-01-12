ff6f43984c4bfffeda6f7739e9bd2c5b02f040e7
“RizkyRasya” <rizkyrasya.m@gmail.com>
2023-01-12 08:40:50 +0700

    nambah routing

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ('Hallo Laravel');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/Home', function () {
+    return ('Halaman Home');
+});
+
+Route::get('/About', function () {
+    return ('Halaman About');
+});
+
+Route::get('/Blog', function () {
+    return ('Halaman Blog');
 });

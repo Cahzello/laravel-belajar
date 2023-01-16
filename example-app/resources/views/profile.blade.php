108cb4796bfa1978108fe373ccd6fce5e633f072
“RizkyRasya” <rizkyrasya.m@gmail.com>
2023-01-16 12:17:00 +0700

    add profile blade

---
+++ example-app/resources/views/profile.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">    <title>Baru</title>
+</head>
+<body>    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+            </div>
+        </div>
+      </nav>
+
+      <div class="jumbotron jumbotron-fluid bg-dark text-light">
+        <div class="container">
+          <h1 class="display-4">Halo!</h1>
+          <p class="lead">Nama Saya Rizky Rasya.</p>
+          <p class="lead">Saya adalah seorang programmer.</p>
+        </div>
+      </div>
+
+      <div class="jumbotron bg-dark text-light">
+        <h1 class="display-4">Join!</h1>
+        <p class="lead">Kami mengembangkan projek bersama-sama</p>
+        <hr class="my-3">
+        <p>Join komunitas kami SEKARANG!</p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Join</a>
+        </p>
+      </div>
+</body>
+</html>

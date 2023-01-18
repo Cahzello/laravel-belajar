85f98d455531413760261cc22d4bb0908a149d39
Cahzello <rizkyrasya.m@gmail.com>
2023-01-18 14:22:00 +0700

    add img

--- example-app/resources/views/profile.blade.php
+++ example-app/resources/views/profile.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">    <title>Baru</title>
+    <link rel="stylesheet" href="file.css">
 </head>
 <body>    
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -45,6 +46,7 @@
         <div class="container">
             <h1 class="display-4">Join!</h1>
         <p class="lead">Kami mengembangkan projek bersama-sama</p>
+        <img src="992680.jpg" alt="YukinaMinato" width="300px">
         <hr class="my-3">
         <p>Join komunitas kami SEKARANG!</p>
         <p class="lead">
